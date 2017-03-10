101e4b3f02dfea7196ea408bfbac22f2c60665a9
bob <bob@bobvann.noip.me>
2017-03-10 20:56:55 +0100

    Use stdout for errors and state, download all zones, not just first 20

--- cf-dump.js
+++ cf-dump.js
@@ -1,13 +1,14 @@
 #!/usr/bin/env node
 
+const MAX_ZONES_PER_PAGE = 50;
+const CSV_FIELD_DELIMITER = ",";
 const URL = "api.cloudflare.com";
 
 const args = process.argv.slice(2);
 if(args.length < 2 ){
-	console.log("Missing arguments");
-	console.log("");
-	console.log("Usage:  ./cf-dump.js <email> <api key>")
-	console.log("Example: ./cf-dump.js email@example.com 2d020di24n2j2kd9di2j2eh82ndwdfdssi2ei")
+	process.stderr.write("Missing arguments\n\n");
+	process.stderr.write("Usage:  ./cf-dump.js <email> <api key>\n")
+	process.stderr.write("Example: ./cf-dump.js email@example.com 2d020di24n2j2kd9di2j2eh82ndwdfdssi2ei\n\n")
 	process.exit(3);
 }
 
@@ -31,18 +32,16 @@ var httpsGet = function(opts, cb){
 				var x = JSON.parse(t);
 
 				cb(x);
-			}catch(e){
-
-			}
+			}catch(e){ }
 
 	  });
 	}).end();
 }
 
-var getAllZonesOptions = function(){
+var getAllZonesOptions = function(pageNo){
 	return {
 	  host: URL, port: 443, method: 'GET',
-	  path: '/client/v4/zones?&status=active&match=all',
+	  path: '/client/v4/zones?&status=active&match=all&per_page='+MAX_ZONES_PER_PAGE+'&page='+pageNo,
 	  headers:{ 'X-Auth-Email': EMAIL, 'X-Auth-Key': APIKEY }
 	};
 }
@@ -84,21 +83,21 @@ function completedAll(zones){
 			var row = "";
 
 			row += z.name;
-			row += ",";
+			row += CSV_FIELD_DELIMITER;
 			row += ts.since;
-			row += ",";
+			row += CSV_FIELD_DELIMITER;
 			row += ts.until;
-			row += ",";
+			row += CSV_FIELD_DELIMITER;
 			row += ts.requests.all;
-			row += ",";
+			row += CSV_FIELD_DELIMITER;
 			row += ts.requests.cached;
-			row += ",";
+			row += CSV_FIELD_DELIMITER;
 			row += ts.requests.uncached;
-			row += ",";
+			row += CSV_FIELD_DELIMITER;
 			row += ts.bandwidth.all;
-			row += ",";
+			row += CSV_FIELD_DELIMITER;
 			row += ts.bandwidth.cached;
-			row += ",";
+			row += CSV_FIELD_DELIMITER;
 			row += ts.bandwidth.uncached;
 
 			console.log(row);
@@ -107,24 +106,38 @@ function completedAll(zones){
 
 }
 
-//script start
-httpsGet(getAllZonesOptions(), function(o){
-	if(!o.success){
-		console.log("Error during fetching zones");
-		console.log( JSON.strinfigy(o.errors ));
-		process.exit(1);
-	}
 
-	var zones = clearAllZones(o);
+getAllZones = function(pageIndex, prevZones, cb){
+	process.stderr.write("Downloading zones list: page " + pageIndex + " (already downloaded " + prevZones.length +" zones)\n" );
+	httpsGet(getAllZonesOptions(pageIndex), function(o){
+		if(!o.success){
+			process.stderr.write("Error during fetching zones\n");
+			process.stderr.write( JSON.strinfigy(o.errors +'\n' ));
+			process.exit(1);
+		}
 
+		var zones = clearAllZones(o);
+		var allZones = prevZones.concat(zones);
+
+		if(zones.length==50){
+			getAllZones(pageIndex+1, allZones, cb);
+		}else{
+			process.stderr.write("Zones list download completed. Downloaded " + allZones.length + " zones\n\n" );
+			cb(allZones);
+		}
+	});
+}
+
+getAllZones(1,[], function(zones){
 	var n=0;
 
 	zones.forEach(function(z,i){
+		process.stderr.write('Downloading statistics for zone  ' + (i+1) + '/' + zones.length +  ' - ' + z.id + ' - ' + z.name + '\n');
 		httpsGet(getZoneOptions(z.id), function(o){
 
 			if(!o.success){
-				console.log("Error during fetching zone");
-				console.log( JSON.stringify(o.errors ));
+				process.stderr.write("Error during fetching zone\n");
+				process.stderr.write( JSON.stringify(o.errors +'\n' ));
 				process.exit(2);
 			}
 
@@ -138,3 +151,4 @@ httpsGet(getAllZonesOptions(), function(o){
 		});
 	});
 });
+
